191ba0e2daec9d4134924bad6d670d2fabc7b66a
Jay <atmajaya.dw@gmail.com>
2025-05-07 16:01:05 +0700

    Create sample REST API

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
-
+use App\Models\ArticleNews;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//sample API
+Route::get('/api/articles', function () {
+    return response()->json(ArticleNews::all());
+});
+
+Route::get('/api/articles/featured', function () {
+    return response()->json(
+        ArticleNews::where('is_featured', 'featured')->get()
+    );
 });
